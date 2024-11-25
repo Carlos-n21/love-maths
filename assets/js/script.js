bad196aeb41fe2953f9a3cf703d8b7a651d7901d
Carlos Neves <c.nsm.714@gmail.com>
2024-11-25 15:00:36 +0000

    Add code to increment score and incorrect answers amount

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,8 +46,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right :D");
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -70,11 +72,24 @@ function calculateCorrectAnswer() {
     }
 }
 
+
+/**
+ * gets current score from DOM and increments value by 1
+ */
 function incrementScore() {
+    
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
+/**
+ * gets current total of incorrect score from DOM and increments value by 1
+ */
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
